cd84d576071f6c143a15608d083078603fda0566
miguelmariagp <m.pereira@wustl.edu>
2016-02-26 14:07:36 -0600

    First exercise
    Still need to sink the figures to a pdf

--- PS4.R
+++ PS4.R
@@ -1,3 +1,4 @@
+install.packages("rvest")
 library(rvest)    
 wikiURL <- 'https://en.wikipedia.org/wiki/List_of_United_States_presidential_elections_by_popular_vote_margin'
 
@@ -7,4 +8,115 @@ wikiURL <- 'https://en.wikipedia.org/wiki/List_of_United_States_presidential_ele
 temp <- wikiURL %>% 
   read_html %>%
   html_nodes("table")
+
+#The table we want is the second one on the page.
+tab.list<-html_table(temp[2])
+
+
+
+#Transforming into dataframe and removing row 2 
+tab<-data.frame(tab.list[[1]][-c(1,2),])
+
+names(tab)<-c("Number","Year","Winner","PartyW","VoteShare","VoteShareMargin",
+              "Vote","VoteMargin","Runner-up","PartyR","Turnout")
+
+
+# The columns with margins are all messy so I need clean them
+
+########################################################
+#Cleaning VoteShare
+########################################################
+tab$VoteShare<-as.numeric(gsub("%","",tab$VoteShare))
+
+
+########################################################
+#Cleaning VoteShareMargin
+########################################################
+
+#There are three types of data
+#Second, for negative percentages, strings include â
+
+#Hence, I use grep() to create a variable that identifies negative margins
+tab$NegShareMargin<-ifelse(1:dim(tab)[1] %in% grep("â",tab$VoteShareMargin),-1,1)
+
+#Now I start by making the characters associated with negative values look similar to the others
+tab$VoteShareMargin<-gsub(".*'","",tab$VoteShareMargin)
+
+#Next I extract everything before the first % sign and make it numeric
+tab$VoteShareMargin<-as.numeric(gsub("%.*","",test))
+
+#And finally I add the sign to the negative values
+tab$VoteShareMargin<-tab$VoteShareMargin*tab$NegShareMargin
+
+
+########################################################
+#Cleaning Vote (absolute number)
+########################################################
+tab$Vote<-as.numeric(gsub(",","",tab$Vote))
+
+########################################################
+#Cleaning Turnout
+########################################################
+tab$Turnout<-as.numeric(gsub("%","",tab$Turnout))
+
+######################################
+##NOTE: CAN'T FIND A PATTERN IN VOTEMARGIN SO I CAN CLEAN IT
+#####################################
+
+#Questions to visualize
+
+##########################################################
+#Did elections became more competitive with time?
+##########################################################
+plot(tab$Year,tab$VoteShareMargin, main="Margin of victory, by election and party",
+     xlab="Election", ylab="Margin (percentage points)")
+lines(loess.smooth(tab$Year,tab$VoteShareMargin, span=1/2),col="gray",lwd=2)
+#lines(loess.smooth(tab$Year[tab$PartyW=="Dem."],tab$VoteShareMargin[tab$PartyW=="Dem."],
+ #                  span=1/2),col="blue",lwd=1)
+#lines(loess.smooth(tab$Year[tab$PartyW=="Rep."],tab$VoteShareMargin[tab$PartyW=="Rep."],
+ #                  span=1/2),col="Red",lwd=1)
+abline(0,0,lty=3)
+points(tab$Year[tab$PartyW=="Rep."],
+       tab$VoteShareMargin[tab$PartyW=="Rep."], col="Red",pch=19)
+points(tab$Year[tab$PartyW=="Dem."],
+       tab$VoteShareMargin[tab$PartyW=="Dem."], col="blue",pch=19)
+legend("bottom",legend=c("Smoother", "Democrats","Republicans"),
+       ncol=3, lwd=c(2,1,1), col=c("gray","blue","Red"), bty="n",
+       lty=c(1,NA,NA),pch=c(NA,19,19))
+
+
+#################################################################
+#Are there differences in the margin of victory by party?
+#################################################################
+tabbipart<-tab[tab$PartyW=="Dem."|tab$PartyW=="Rep.",]
+
+par(mfrow=c(1,3))
+boxplot(VoteShareMargin~PartyW,data=tabbipart,ylim=c(-5,30),col=c("blue","Red"),
+        xaxt="n",ylab="Margin of Victory (percentage points)",xlab="1824-2012")
+abline(0,0,lty=3)
+boxplot(VoteShareMargin~PartyW,data=tabbipart[tabbipart$Year<=1968,],ylim=c(-5,30),
+        xaxt="n",yaxt="n",xlab="Pre-Civil Rights Movement",col=c("blue","Red"))
+abline(0,0,lty=3)
+boxplot(VoteShareMargin~PartyW,data=tabbipart[tabbipart$Year>1968,],ylim=c(-5,30),
+    xaxt="n",yaxt="n",xlab="Post-Civil Rights Movement",col=c("blue","Red"))
+abline(0,0,lty=3)
+
+
+##############################################
+#Total number of votes and turnout by time
+##############################################
+#Ordering the dataset by Year
+tab<-tab[with(tab,order(Year)),]
+
+par(mfrow=c(1,1),mar=c(5,4,4,5))
+plot(tab$Year,log(tab$Vote),type="p",xlab="Election",ylab="# Votes for winner (logged)",
+     main="Votes for Winner and Turnout, by Election",pch=16,col="Navyblue")
+par(new=TRUE)
+plot(tab$Year,tab$Turnout,type="l",col="Gold",xaxt="n",yaxt="n",xlab="",ylab="",lwd=1.5)
+axis(4)
+mtext("Turnout (%)",side=4,line=3)
+legend("bottomright",legend=c("Votes for winner (log)", "Turnout (%)"),
+        lwd=c(1,1.5), col=c("Navyblue","Gold"), bty="n", lty=c(NA,1),pch=c(16,NA))
+
+
 
